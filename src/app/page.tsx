3a154ad1129152f36cfddb82e7161c34f52119e0
Divyesh Patel <dev.eth007@gmail.com>
2023-11-08 18:56:38 +0530

    Add changes in root page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,6 @@ export default async function Home() {
       </div>
       <div className='my-4 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4'>
       {products.slice(1).map((product) =><ProductCard product={product} key={product.id} /> )}
-
       </div>
     </div>
   )
